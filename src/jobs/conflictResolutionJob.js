c31545edcf960766646d7cd89d20d94eb38ee3ff
Artyom Kuznetsov <krosent2@gmail.com>
2022-08-09 22:27:55 +0200

    feat: display rules from Solid POD

--- src/jobs/conflictResolutionJob.js
+++ src/jobs/conflictResolutionJob.js
@@ -10,10 +10,10 @@ exports.initConflictResolutionJob = () => {
 
     rulesWithUsername.forEach(async (ruleWithUsername) => JSON.parse(ruleWithUsername.rules).forEach(async (rule) => {
       const query = {
-        username: ruleWithUsername.username, description: rule.description, device: rule.device, trigger: rule.trigger, category: rule.category,
+        username: ruleWithUsername.username, description: rule.description, device: rule.device, trigger: rule.trigger, action: rule.action, category: rule.category,
       };
       const update = {
-        username: ruleWithUsername.username, description: rule.description, device: rule.device, trigger: rule.trigger, category: rule.category,
+        username: ruleWithUsername.username, description: rule.description, device: rule.device, trigger: rule.trigger, action: rule.action, category: rule.category,
       };
       const options = { upsert: true, new: true, setDefaultsOnInsert: true };
       await Rule.findOneAndUpdate(query, update, options).catch((err) => console.log(`error is here: ${err}`));
